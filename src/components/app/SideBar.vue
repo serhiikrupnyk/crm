9a5246086411d626cfecf334e0ae36facc2ea560
SerhiyKr <44681507+SerhiyKr@users.noreply.github.com>
2022-07-11 01:51:16 +0300

    add date filters

---
+++ src/components/app/SideBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: value}">
+    <router-link
+        tag="li"
+        v-for="link in links"
+        :key="link.url"
+        :to="link.url"
+        :exact="link.exact"
+        active-class="active"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{ link.title }}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  props: ['value'],
+  data: () => ({
+    links: [
+      {title: 'Account', url: '/', exact: true},
+      {title: 'History', url: '/history'},
+      {title: 'Planning', url: '/planning'},
+      {title: 'New record', url: '/record'},
+      {title: 'Categories', url: '/categories'}
+    ]
+  })
+}
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,72 +1,31 @@
 <template>
   <div class="app-main-layout">
-    <nav class="navbar green lighten-1">
-      <div class="nav-wrapper">
-        <div class="navbar-left">
-          <a href="#">
-            <i class="material-icons black-text">dehaze</i>
-          </a>
-          <span class="black-text">12.12.12</span>
-        </div>
+    <SideBar v-model="toggleShowSidebar" />
+    <NavBar @click="toggleShowSidebar = !toggleShowSidebar" />
 
-        <ul class="right hide-on-small-and-down">
-          <li>
-            <a
-                class="dropdown-trigger black-text"
-                href="#"
-                data-target="dropdown"
-            >
-              USER NAME
-              <i class="material-icons right">arrow_drop_down</i>
-            </a>
-
-            <ul id='dropdown' class='dropdown-content'>
-              <li>
-                <a href="#" class="black-text">
-                  <i class="material-icons">account_circle</i>Профиль
-                </a>
-              </li>
-              <li class="divider" tabindex="-1"></li>
-              <li>
-                <a href="#" class="black-text">
-                  <i class="material-icons">assignment_return</i>Выйти
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    <ul class="sidenav app-sidenav open">
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Счет</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">История</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Планирование</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Новая запись</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Категории</a>
-      </li>
-    </ul>
-
-    <main class="app-content">
+    <main class="app-content" :class="{full: !toggleShowSidebar}">
       <div class="app-page">
         <router-view/>
       </div>
     </main>
 
     <div class="fixed-action-btn">
-      <a class="btn-floating btn-large blue" href="#">
+      <router-link class="btn-floating btn-large blue" to="/record">
         <i class="large material-icons">add</i>
-      </a>
+      </router-link>
     </div>
   </div>
 </template>
 
+<script>
+import NavBar from "@/components/app/NavBar";
+import SideBar from "@/components/app/SideBar";
+
+export default {
+  name: 'MainLayout',
+  data: () => ({
+    toggleShowSidebar: true
+  }),
+  components: {SideBar, NavBar}
+}
+</script>
