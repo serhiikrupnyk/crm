e415d5d8c83bcda79130a6ef57874fae61ac63c6
SerhiyKr <44681507+SerhiyKr@users.noreply.github.com>
2022-08-01 21:28:52 +0300

    [ADD] login & logout actions

---
+++ src/store/auth.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/compat/app";
+
+export default {
+    actions: {
+        async login({dispatch, commit}, {email, password}) {
+            try {
+                await firebase.auth().signInWithEmailAndPassword(email, password)
+            } catch (e) {
+                throw e
+            }
+        },
+        async logout() {
+            await firebase.auth().signOut()
+        }
+    }
+}
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -68,7 +68,7 @@ export default {
     }
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -79,9 +79,10 @@ export default {
         password: this.password
       }
 
-      console.log(formData)
-
-      this.$router.push('/')
+      try {
+        await this.$store.dispatch('login', formData)
+        this.$router.push('/')
+      } catch (e) {}
     }
   }
 }
