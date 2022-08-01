18f17c1977b3cfca79a836bf599c6ae09af07748
SerhiyKr <44681507+SerhiyKr@users.noreply.github.com>
2022-08-01 18:41:07 +0300

    [ADD] initialize firebase

--- src/main.js
+++ src/main.js
@@ -7,14 +7,37 @@ import dateFilter from "@/filters/date.filter"
 import router from './router'
 import store from './store'
 
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"
+import "firebase/compat/database"
+
 Vue.config.productionTip = false
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdFyqBjiub_iktK4cpN2HxiKnWa51Mr_s",
+  authDomain: "crm-bookkeeping.firebaseapp.com",
+  projectId: "crm-bookkeeping",
+  storageBucket: "crm-bookkeeping.appspot.com",
+  messagingSenderId: "802342655564",
+  appId: "1:802342655564:web:d4811df403f1e7b7ab06e9",
+  measurementId: "G-K90YVQ4TQJ"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
