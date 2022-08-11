ce3cce95294358ac6e63663c630ba84ac406ca73
SerhiyKr <44681507+serhiikrupnyk@users.noreply.github.com>
2022-08-11 22:40:57 +0300

    [FIX] navbar style

--- src/components/app/NavBar.vue
+++ src/components/app/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar transparent">
+  <nav class="navbar green accent-2">
     <div class="nav-wrapper">
       <div class="navbar-left">
         <a @click.prevent="$emit('click')" href="#">
